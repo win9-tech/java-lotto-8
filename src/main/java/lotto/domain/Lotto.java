4551349e480d11b200b53e4190483558a9bce6f9
SeungGyu <phillipskl123@gmail.com>
2025-11-03 15:39:14 +0900

    feat: 로또 중복 번호 검증 예외 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,9 @@ package lotto.domain;
 import lotto.util.constant.ErrorMessages;
 import lotto.util.constant.LottoRules;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
@@ -21,6 +23,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LottoRules.PICK_COUNT) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_NUMBER_COUNT);
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessages.WIN_DUPLICATE);
         }
     }
 }
