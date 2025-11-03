95f73f2845df4ab01b5d0308d4bc9e3176dfcecd
SeungGyu <phillipskl123@gmail.com>
2025-11-03 14:40:55 +0900

    refactor: 입출력 포멧 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,11 +10,13 @@ public class InputView {
     }
 
     public String requestWinNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public String requestBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -11,6 +11,8 @@ public class OutputView {
     private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
 
     public static final String RESULT_FORMAT = """
+                당첨 통계
+                ---
                 3개 일치 (5,000원) - %d개
                 4개 일치 (50,000원) - %d개
                 5개 일치 (1,500,000원) - %d개
@@ -34,6 +36,7 @@ public class OutputView {
     }
 
     public void printResult(Map<LottoRank, Integer> rankCount, double record) {
+        System.out.println();
         System.out.printf(RESULT_FORMAT,
                 rankCount.getOrDefault(LottoRank.FIFTH, 0),
                 rankCount.getOrDefault(LottoRank.FOURTH, 0),
